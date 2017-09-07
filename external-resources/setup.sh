c62a0ab4db7d22c96983ce2b43f1ceece5e1a860
Paul Tegelaar <Gebruiker@LAPTOP-PROGAIA>
2017-09-07 19:56:56 +0200

    Try, try and try again

--- external-resources/setup.sh
+++ external-resources/setup.sh
@@ -1,31 +1,47 @@
-#!/bin/sh
+#!/bin/bash
 
-installdir=$(pwd -P)
+if env | grep -q ^LIQUID_HOME=
+ then
+  echo LIQUID_HOME IS defined
+ else
+	echo LIQUID_HOME is not defined
+	echo This environment variable is needed to run this program
+	exit
+fi
 
-rm -rf $installdir/config/liquid-server.jks
+if env | grep -q ^JAVA_HOME=
+then
+ echo JAVA_HOME IS defined
+else
+ echo JAVA_HOME is not defined
+ echo This environment variable is needed to run this program
+ exit
+fi
 
-rm -rf $installdir/config/liquid-server.crt
+rm -rf $LIQUID_HOME/config/liquid-server.jks
 
-rm -rf $installdir/config/liquid-client.crt
+rm -rf $LIQUID_HOME/config/liquid-server.crt
 
-rm -rf $installdir/config/liquid-client.jks
+rm -rf $LIQUID_HOME/config/liquid-client.crt
 
-rm -rf $installdir/config/liquid-client-trust.jks
+rm -rf $LIQUID_HOME/config/liquid-client.jks
 
-rm -rf $installdir/config/liquid-server-trust.jks
+rm -rf $LIQUID_HOME/config/liquid-client-trust.jks
+
+rm -rf $LIQUID_HOME/config/liquid-server-trust.jks
 
 pass=$(date +%s | sha256sum | base64 | head -c 32 ; echo)
 
-echo $pass>$installdir/config/password.txt
+echo $pass>$LIQUID_HOME/config/password.txt
 
-$JAVA_HOME/bin/keytool -genkey -alias liquid -keyalg RSA -keystore $installdir/config/liquid-server.jks -storepass $pass -keypass $pass -dname "cn=Liquid, ou=Liquid, o=Liquid, c=NL" -validity 10000
+$JAVA_HOME/bin/keytool -genkey -alias liquid -keyalg RSA -keystore $LIQUID_HOME/config/liquid-server.jks -storepass $pass -keypass $pass -dname "cn=Liquid, ou=Liquid, o=Liquid, c=NL" -validity 10000
 
-$JAVA_HOME/bin/keytool -export -alias liquid -keystore $installdir/config/liquid-server.jks -file $installdir/config/liquid-server.crt -storepass $pass -keypass $pass
+$JAVA_HOME/bin/keytool -export -alias liquid -keystore $LIQUID_HOME/config/liquid-server.jks -file $LIQUID_HOME/config/liquid-server.crt -storepass $pass -keypass $pass
 
-$JAVA_HOME/bin/keytool -genkey -alias liquid-client -keyalg RSA -keystore $installdir/config/liquid-client.jks -storepass $pass -keypass $pass -dname "cn=Liquid-client, ou=Liquid, o=Liquid, c=NL" -validity 10000
+$JAVA_HOME/bin/keytool -genkey -alias liquid-client -keyalg RSA -keystore $LIQUID_HOME/config/liquid-client.jks -storepass $pass -keypass $pass -dname "cn=Liquid-client, ou=Liquid, o=Liquid, c=NL" -validity 10000
 
-$JAVA_HOME/bin/keytool -export -alias liquid-client -keystore $installdir/config/liquid-client.jks -file $installdir/config/liquid-client.crt -storepass $pass -keypass $pass
+$JAVA_HOME/bin/keytool -export -alias liquid-client -keystore $LIQUID_HOME/config/liquid-client.jks -file $LIQUID_HOME/config/liquid-client.crt -storepass $pass -keypass $pass
 
-$JAVA_HOME/bin/keytool -import -alias liquid -keystore $installdir/config/liquid-client-trust.jks -file $installdir/config/liquid-server.crt -storepass $pass -keypass $pass -noprompt
+$JAVA_HOME/bin/keytool -import -alias liquid -keystore $LIQUID_HOME/config/liquid-client-trust.jks -file $LIQUID_HOME/config/liquid-server.crt -storepass $pass -keypass $pass -noprompt
 
-$JAVA_HOME/bin/keytool -import -alias liquid-client -keystore $installdir/config/liquid-server-trust.jks -file $installdir/config/liquid-client.crt -storepass $pass -keypass $pass -noprompt
+$JAVA_HOME/bin/keytool -import -alias liquid-client -keystore $LIQUID_HOME/config/liquid-server-trust.jks -file $LIQUID_HOME/config/liquid-client.crt -storepass $pass -keypass $pass -noprompt
