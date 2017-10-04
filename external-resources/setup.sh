889d6221ae747bcc3f9f68dc2f3729226b43dd2e
Paul Tegelaar <Gebruiker@LAPTOP-PROGAIA>
2017-10-04 11:05:27 +0200

    Setup aangepast omdat postinstall niet goed werkt

--- external-resources/setup.sh
+++ external-resources/setup.sh
@@ -34,18 +34,18 @@ $JDK_HOME/bin/keytool -import -alias liquid -keystore $LIQUID_HOME/config/liquid
 
 $JDK_HOME/bin/keytool -import -alias liquid-client -keystore $LIQUID_HOME/config/liquid-server-trust.jks -file $LIQUID_HOME/config/liquid-client.crt -storepass $pass -keypass $pass -noprompt
 
-copy1=cp /opt/liquid-os/LiquidService /etc/init.d/
+copy1= cp /opt/liquid-os/LiquidService /etc/init.d/
 
 echo "Copied service file to init.d $copy1"
 
-chmod1=chmod +x /etc/init.d/LiquidService
+chmod1= chmod +x /etc/init.d/LiquidService
 
 echo "Added execute rights to service file $chmod1"
 
-chmod2=chmod +x /opt/liquid-os/setup.sh
+chmod2= chmod +x /opt/liquid-os/setup.sh
 
 echo "Added execute rights to setup file $chmod2"
 
-chown1=chown -R liquid:liquid /opt/liquid-os/
+chown1= chown -R liquid:liquid /opt/liquid-os/
 
 echo "Changed ownership of home dir $chown1"
