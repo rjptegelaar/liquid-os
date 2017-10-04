e8904d0457495b8ba0c34abd047a402437626775
Paul Tegelaar <Gebruiker@LAPTOP-PROGAIA>
2017-10-04 09:29:44 +0200

    Setup and java_home fix

--- external-resources/setup.sh
+++ external-resources/setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo "Please enter JDK home: "
+read JDK_HOME
+echo "JDK home set to: $JDK_HOME"
+
+LIQUID_HOME="$PWD"
+
 rm -rf $LIQUID_HOME/config/liquid-server.jks
 
 rm -rf $LIQUID_HOME/config/liquid-server.crt
@@ -16,14 +22,14 @@ pass=$(date +%s | sha256sum | base64 | head -c 32 ; echo)
 
 echo $pass>$LIQUID_HOME/config/password.txt
 
-$JAVA_HOME/bin/keytool -genkey -alias liquid -keyalg RSA -keystore $LIQUID_HOME/config/liquid-server.jks -storepass $pass -keypass $pass -dname "cn=Liquid, ou=Liquid, o=Liquid, c=NL" -validity 10000
+$JDK_HOME/bin/keytool -genkey -alias liquid -keyalg RSA -keystore $LIQUID_HOME/config/liquid-server.jks -storepass $pass -keypass $pass -dname "cn=Liquid, ou=Liquid, o=Liquid, c=NL" -validity 10000
 
-$JAVA_HOME/bin/keytool -export -alias liquid -keystore $LIQUID_HOME/config/liquid-server.jks -file $LIQUID_HOME/config/liquid-server.crt -storepass $pass -keypass $pass
+$JDK_HOME/bin/keytool -export -alias liquid -keystore $LIQUID_HOME/config/liquid-server.jks -file $LIQUID_HOME/config/liquid-server.crt -storepass $pass -keypass $pass
 
-$JAVA_HOME/bin/keytool -genkey -alias liquid-client -keyalg RSA -keystore $LIQUID_HOME/config/liquid-client.jks -storepass $pass -keypass $pass -dname "cn=Liquid-client, ou=Liquid, o=Liquid, c=NL" -validity 10000
+$JDK_HOME/bin/keytool -genkey -alias liquid-client -keyalg RSA -keystore $LIQUID_HOME/config/liquid-client.jks -storepass $pass -keypass $pass -dname "cn=Liquid-client, ou=Liquid, o=Liquid, c=NL" -validity 10000
 
-$JAVA_HOME/bin/keytool -export -alias liquid-client -keystore $LIQUID_HOME/config/liquid-client.jks -file $LIQUID_HOME/config/liquid-client.crt -storepass $pass -keypass $pass
+$JDK_HOME/bin/keytool -export -alias liquid-client -keystore $LIQUID_HOME/config/liquid-client.jks -file $LIQUID_HOME/config/liquid-client.crt -storepass $pass -keypass $pass
 
-$JAVA_HOME/bin/keytool -import -alias liquid -keystore $LIQUID_HOME/config/liquid-client-trust.jks -file $LIQUID_HOME/config/liquid-server.crt -storepass $pass -keypass $pass -noprompt
+$JDK_HOME/bin/keytool -import -alias liquid -keystore $LIQUID_HOME/config/liquid-client-trust.jks -file $LIQUID_HOME/config/liquid-server.crt -storepass $pass -keypass $pass -noprompt
 
-$JAVA_HOME/bin/keytool -import -alias liquid-client -keystore $LIQUID_HOME/config/liquid-server-trust.jks -file $LIQUID_HOME/config/liquid-client.crt -storepass $pass -keypass $pass -noprompt
+$JDK_HOME/bin/keytool -import -alias liquid-client -keystore $LIQUID_HOME/config/liquid-server-trust.jks -file $LIQUID_HOME/config/liquid-client.crt -storepass $pass -keypass $pass -noprompt
